7b239ed5611b22b83077ae42e98e8597954bfd82
JayThummar024 <thummar024@gmail.com>
2025-10-26 12:52:19 +0000

    chnaged resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="./assets/jay-thummar-resume.pdf"
+                href="../../public/jay-thummar-resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 download=".jay-thummar-resume.pdf"
               >
